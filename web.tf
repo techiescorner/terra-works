1428fed157390b61136905a58de051ab5b00dd50
techiescorner <arshakfm@gmail.com>
2019-11-02 18:36:46 +0530

    To provision DB instance in private subnet

--- web.tf
+++ web.tf
@@ -37,17 +37,11 @@ cidr_blocks = ["0.0.0.0/0"]
  }
 
 egress {
-from_port = 3306
-to_port = 3306
-protocol = "tcp"
-cidr_blocks = ["10.0.2.0/24"] 
-}
-egress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+   }
 vpc_id = "${aws_vpc.terraform-vpc.id}"
 
 tags = {
